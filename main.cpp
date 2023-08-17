17946b98df6ee61e29e7c2150bbc8ec51b3fda25
Mohamed Ali Youssef <43966369+mohamed-ali-youssef@users.noreply.github.com>
2023-08-17 16:02:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,50 @@
 #include <iostream>
+#include <stdlib.h>
 #include "drawing.h"
 using namespace std;
 Drawing dr;
+
+void Project_Column_Chart()
+{
+    cout << "\n\n\t" << dr.TextColor("Library for creating graphs using Terminal",
+    dr.FrontColor.Cyan(),
+    dr.Background.Default(),
+    dr.FormatAttribute.Bold()) << "\n\n";
+
+    char strRow[] = "Sat,Sum,Mon,Tue,Wed,Thu,GGG";
+    char strCol[] = "75,40,65,98,20,60,100";
+    dr.Column_Chart(strRow,
+    strCol,
+    dr.Background.Red(),
+    dr.FrontColor.Yellow(),
+    dr.Background.Green());
+
+    cout << "\t\t" << dr.TextColor("Profits for the first week",
+    dr.FrontColor.Cyan(),
+    dr.Background.Default(),
+    dr.FormatAttribute.Bold()) << endl;
+}
+
+void Project_Bar_Chart()
+{
+    char strRow[] = "Sat,Sum,Mon,Tue,Wed,Thu,GGG";
+    char strCol[] = "75,40,65,98,5,60,100";
+
+    dr.Bar_Chart(strRow,
+    strCol,
+    dr.Background.Red(),
+    dr.FrontColor.Yellow(),
+    dr.Background.Green());
+}
+
 
 int main()
 {
     system("clear");
 
-    cout << "\n\n\t" << dr.TextColor("Library for creating graphs using Terminal",
-    dr.FrontColor.Cyan(),
-    dr.Background.Null(),
-    dr.Bold.True()) << "\n\n";
-
-
-    char strRow[] = "Sat,Sum,Mon,Tue,Wed,Thu";
-    char strCol[] = "75,40,65,98,20,60";
-    dr.Chart(strRow,
-    strCol,
-    dr.ColumnsColor.Red(),
-    dr.HeadsColor.Yellow(),
-    dr.ColorBox.Blue());
-
-
-    cout << "\t\t" << dr.TextColor("Profits for the first week",
-    dr.FrontColor.Cyan(),
-    dr.Background.Null(),
-    dr.Bold.True());
-
+    Project_Column_Chart();
+    cout << "\n\n";
+    Project_Bar_Chart();
 
     return 0;
 }
